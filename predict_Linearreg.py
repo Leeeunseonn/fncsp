961c3512eb9500ec152c8621d428b791d3ebbc57
Lee eunseon <37540860+Leeeunseonn@users.noreply.github.com>
2024-10-17 17:17:58 +0900

    업종별 속성 학습 후 연도별 미래 속성 예측 / 선형회귀 (sklearn)

--- predict_Linearreg.py
+++ predict_Linearreg.py
@@ -3,7 +3,7 @@
 if __name__ != "__main__":
     import os
     import sys
-    sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+    sys.path.append(os.path.dirname(os.path.realpath(__file__))) 
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 from cmath import nan
